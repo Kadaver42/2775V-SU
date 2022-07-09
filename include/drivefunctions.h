126c0b41757a5130bb8dc5dc34e0e27612b9cf34
Josh <joshua.bernheisel@gmail.com>
2022-07-08 22:14:12 -0500

    added drive functions and flywheel

--- include/drivefunctions.h
+++ include/drivefunctions.h
@@ -2,6 +2,12 @@ void setDriveVoltage(float leftVolts, float rightVolts);
 
 void setDriveVelocity(float leftVelo, float rightVelo);
 
+float reduceAngle0to360(float angleDeg);
+
+float reduceAngleMinus180to180(float angleDeg);
+
 void updatePosition();
 
 int positionTrack();
+
+void turn(float angle, float timeout, float settlingerror, float settlingtime, float kp, float ki, float kd, float maxvoltage);
--- src/otherfunctions.cpp
+++ src/otherfunctions.cpp
@@ -2,3 +2,8 @@
 
 float shootingtime = 400;
 float unshootingtime = 200;
+
+void bangbangflywheel(float voltage, float velocity) {
+  if (Flywheel.velocity(pct) < velocity-5) {Flywheel.spin(fwd,12, volt);}
+  else {Flywheel.spin(fwd, voltage, volt);}
+}
